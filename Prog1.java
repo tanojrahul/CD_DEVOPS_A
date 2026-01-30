c3030cf3b10aaaad6dea0cded2cab8eae71b9034
B Tanoj Rahul Reddy <tanojrahul19@gmail.com>
2026-01-30 14:52:56 +0530

    Add Prog1.java to print sum of two numbers

---
+++ Prog1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Prog1 {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    // Scanner sc = new Scanner(System.in);
+    // System.out.println("Enter value for a:");
+    // int a = sc.nextInt();
+    int a = 2;
+    // System.out.println("Enter value for b:");
+    // int b = sc.nextInt();
+    int b = 5;
+    int sum = a + b;
+    System.out.println("The sum of a and b is: " + sum);
+
+    System.out.println("Program executed successfully.");
+
+    // sc.close();
+  }
+}
